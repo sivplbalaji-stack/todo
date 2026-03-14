c37aef1180362c67b62bc798363f00ec75f430d5
sivplbalaji-stack <sivp.balaji@gmail.com>
2026-03-14 17:59:13 +0530

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,64 +1,109 @@
-import Image from "next/image";
+"use client"
+import React, { useState, useRef, useEffect } from "react";
+
+export interface Task{
+  id: number,
+  text: String,
+  completed: Boolean
+}
 
 export default function Home() {
+  const [input, setInput] = useState<string>("");
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const inpRef = useRef<HTMLInputElement>(null);
+
+  useEffect(()=>{
+    const storedTasks = localStorage.getItem("tasks");
+    if(storedTasks){
+      setTasks(JSON.parse(storedTasks));
+    } else {
+      setTasks([]);
+    }
+    inpRef.current?.focus();
+  },[]);
+  
+  const addTask = ()=>{
+    if(!input.trim()) return;
+    const newTask: Task = {
+      id: Date.now(),
+      text: input,
+      completed: false
+    }
+
+    setTasks([...tasks, newTask]);
+    setInput("");
+    inpRef.current?.focus();
+  }
+
+  const deleteTask= (id:number) =>{
+    const taskExists = tasks.some(task=> task.id === id);
+    if(!taskExists) return;
+
+    setTasks(tasks.filter(task => task.id !== id));
+  }
+
+  const toggleTask = (id:number)=>{
+    setTasks(
+      tasks.map(task =>
+        task.id === id
+        ? { ...task, completed: !task.completed}
+        : task
+      )
+    )
+
+    setTasks(
+      tasks.map(task =>
+        task.id === id
+          ? { ...task, completed: !task.completed }
+          : task
+      )
+    )
+  }
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <main className="bg-white p-6 rounded-lg shadow-lg w-96">
+        
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+          <h1 className="text-2xl font-bold text-center mb-4">ToDo list</h1>
+          <div className="flex gap-2">
+              <input 
+                ref={inpRef}
+                type="text" 
+                value={input} 
+                onChange={(e) =>{setInput(e.target.value)}}
+                className="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 flex-1"
+              />
+
+              <button 
+                onClick={addTask}
+                className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+              >
+                Add Task
+              </button>
+          </div>
+          <ul className="mt-4 space-y-2">
+              {
+                tasks.map(task => (
+                  <li key={task.id} className=" justify-between items-center bg-gray-50 p-2 rounded">
+                    <span 
+                      onClick={() => toggleTask(task.id)} 
+                      className={`cursor-pointer ${task.completed ? "line-through text-gray-500" : ""}`}
+                    >
+                      {task.text}
+                    </span>
+                    <button 
+                      className="text-white bg-red-500 px-3 py-1 rounded hover:bg-red-600 ml-2" 
+                      onClick={() => deleteTask(task.id)}
+                    >
+                      Delete
+                    </button>
+                  </li>
+                  )
+                )
+              }
+            </ul>
+          </div>
       </main>
     </div>
   );
